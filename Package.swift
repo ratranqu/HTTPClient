3e7259312ebd7d15e5da7618d9e3f60ea12b09f8
Alex Tran Qui <ratranqu@users.noreply.github.com>
2017-10-01 22:52:27 +0200

    update TCP

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/ratranqu/Venice.git", majorVersion: 0, minor: 14),
         .Package(url: "https://github.com/ratranqu/HTTPFile.git", majorVersion: 0, minor: 14),
-        .Package(url: "https://github.com/Zewo/TCP.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/ratranqu/TCP.git", majorVersion: 0, minor: 14),
     ]
 )
